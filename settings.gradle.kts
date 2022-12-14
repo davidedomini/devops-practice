b82cd44031046c8ece9c77569d5db7878f7dd691
dom <davide.domini2@unibo.it>
2022-12-14 19:45:41 +0100

    build: add conventional commits hook

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.23"
+}
+
 rootProject.name = "devops-practice"
 
+
+gitHooks {
+    commitMsg {
+        conventionalCommits()
+    }
+    createHooks(true)
+}
