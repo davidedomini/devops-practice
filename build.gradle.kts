d08717bf7dc0cc4d24259aeb022f215154ee956f
dom <davide.domini2@unibo.it>
2022-12-22 12:31:56 +0100

    ci: add test release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.6.21"
+    id("org.jetbrains.kotlin.jvm") version "1.7.22"
     application
+    id("org.jetbrains.dokka") version "1.7.20"
+    id ("org.danilopianini.publish-on-central") version "2.0.11"
 }
 
-group = "it.unibo.lss"
-version = "1.0-SNAPSHOT"
+group = "io.github.davidedomini"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -22,4 +24,33 @@ tasks.getByName<Test>("test") {
 
 application {
     mainClass.set("it.unibo.lss.HelloClassKt")
+}
+
+publishOnCentral {
+    projectUrl.set("https://github.com/davidedomini/devops-practice")
+    scmConnection.set("git:git@github.com:davidedomini/devops-practice")
+}
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            pom {
+                developers {
+                    developer {
+                        name.set("Davide Domini")
+                        email.set("davide.domini@studio.unibo.it")
+                        url.set("https://davidedomini.github.io/")
+                    }
+                }
+            }
+        }
+    }
+}
+
+if(System.getenv("CI") == true.toString()) {
+    signing {
+        val signingKey: String? by project
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
 }
